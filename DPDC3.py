23b7ea8faaf6e735918ecf9f192b924a7e76ce96
Maximilian Schneider <mail@maximilianschneider.net>
2013-05-27 11:40:24 +0200

    rewrote dependency inclusion check

--- DPDC3.py
+++ DPDC3.py
@@ -1,35 +1,40 @@
 import sys, re
 import csv
 
-class columnDict(object):
-	dict={}
+tsv_file = openFile(sys.argv[1])
+analyzed_column = int(sys.argv[2])
 
-candidates=range(800)
-del candidates[int(sys.argv[2])]
-instancelist = [ columnDict() for i in range(800)]
+class Candidate:
+	def __init__(self, index):
+		self.dependencies = {}
+		self.index = index
+
+	def test(self, key, row):
+		value = row[self.index]
+		if key in self.dependencies:
+			self.dependencies[key] == value
+		else:
+			self.dependencies[key] = value
+			True
+
+candidates = [Candidate(i) for i in range(800) if i != analyzed_column]
 
 def openFile(filename):
 	return csv.reader(open(filename, "rb"),delimiter='\t')
 
-def main():
-	count = 0
+count = 0
+for row in tsv_file:
+	count = count+1
+	print "Now on line" + str(count)
+	key = row[analyzed_column]
+	for candidate in candidates:
+		if candidate.test(key, row):
+			print "Test successfull"
+		else:
+			print "Test insuccessfull"
+			candidates.remove(candidate)
+	if len(candidates) == 0:
+		break
 
-	tsv_file = openFile(sys.argv[1])
-	for row in tsv_file:
-		count = count+1
-		print "Now on line" + str(count)	
-	
-		for j in candidates:
-			if row[int(sys.argv[2])] in instancelist[j].dict and instancelist[j].dict.get(row[int(sys.argv[2])])!=row[j]:
-				del candidates[j]
-			else:
-				instancelist[j].dict[row[int(sys.argv[2])]]=row[j]
-		
-				
-	
-				
-	for s in candidates:
-		print "C"+sys.argv[2] +"--> C" + s
-	
-if __name__ == '__main__':
-   main()
+for candidate in candidates:
+	print "C"+str(analyzed_column) +"--> C" + str(candidate.index)
