65ddcbb4458bb66bfec8785be1b07326b4b0a1c1
Maximilian Schneider <mail@maximilianschneider.net>
2013-05-28 19:01:21 +0200

    fixed bug

--- DPDCP3.py
+++ DPDCP3.py
@@ -2,17 +2,16 @@ import sys, re
 import csv
 
 def openFile(filename):
-  return csv.reader(open(filename, "r"),delimiter='\t')
+	return csv.reader(open(filename, "r"),delimiter='\t')
 
 def writeTSV(analyzed_column,candidates):
 	tsv_result=open("result.tsv","a")
 	for candidate in candidates:
 		tsv_result.write("C"+str(analyzed_column).zfill(3) +"\tC" + str(candidate.index).zfill(3)+"\n")
 	tsv_result.close()
-	
-	
-	
-tsv_file = openFile(sys.argv[1])
+
+
+
 
 
 class Candidate:
@@ -22,33 +21,25 @@ class Candidate:
 
 	def test(self, key, row):
 		value = row[self.index]
-		if key in self.dependencies and self.dependencies[key] != value:
-			False
+		if key in self.dependencies:
+			return self.dependencies[key] == value
 		else:
 			self.dependencies[key] = value
-			True
-
-
-
+			return True
 
 for analyzed_column in range(800):
 	candidates = [Candidate(i) for i in range(800) if i != analyzed_column]
-	count = 0
+	row_id = 0
+	tsv_file = openFile(sys.argv[1])
 	for row in tsv_file:
-		count = count+1
-		print ("Now on line" + str(count))
 		key = row[analyzed_column]
 		for candidate in candidates:
-			if candidate.test(key, row):
-				print ("Test successfull")
-			else:
-				print ("Test insuccessfull")
-				print (key  + candidate.dependencies[key])
+			if not candidate.test(key, row):
 				candidates.remove(candidate)
-		if len(candidates) == 0:
+		if len(candidates) == 0 or row_id > 5000:
 			break
+		row_id += 1
 
-		
 
 	for candidate in candidates:
 		print ("C"+str(analyzed_column).zfill(3) +"\tC" + str(candidate.index).zfill(3))
